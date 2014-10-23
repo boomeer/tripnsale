36de0081ba56d3f157a95c5957ca4df2b1c12d5f
Ilya Korovin <ilya.krvn@gmail.com>
2014-10-24 03:09:10 +0400

    change design

--- static/offer/main.js
+++ static/offer/main.js
@@ -18,5 +18,37 @@ $(document).ready(function() {
         TripsRefresh();
     });
 
-    $("#allPagesWrapper").fullpage();
+    $("#allPagesWrapper").fullpage({
+        verticalCentered: false,
+        resize : true,
+        anchors:['main', 'trips', 'buys', 'footer'],
+        scrollingSpeed: 700,
+        slidesNavigation: true,
+        slidesNavPosition: 'bottom',
+        loopBottom: false,
+        loopTop: false,
+        loopHorizontal: true,
+        autoScrolling: true,
+        scrollOverflow: false,
+        css3: true,
+        paddingTop: '3em',
+        paddingBottom: '10px',
+        normalScrollElements: '',
+        normalScrollElementTouchThreshold: 5,
+        keyboardScrolling: true,
+        touchSensitivity: 15,
+        continuousVertical: false,
+        animateAnchor: true,
+        sectionSelector: '.section',
+        slideSelector: '.slide',
+        responsive: 0,
+
+        //events
+        onLeave: function(index, nextIndex, direction){},
+        afterLoad: function(anchorLink, index){},
+        afterRender: function(){},
+        afterResize: function(){},
+        afterSlideLoad: function(anchorLink, index, slideAnchor, slideIndex){},
+        onSlideLeave: function(anchorLink, index, slideIndex, direction){}
+    });
 });
--- user/models.py
+++ user/models.py
@@ -1,5 +1,6 @@
 from django.db import models
 import django.contrib.auth.models as authModels
+import tripnsale.settings as settings
 
 
 class User(authModels.User):
@@ -7,3 +8,7 @@ class User(authModels.User):
     regRemoteAddr = models.TextField(default="")
     country = models.TextField(default="")
     city = models.TextField(default="")
+    avatar = models.ImageField(upload_to="avatars", blank=True, null=True)
+
+    def avatarUrl(self):
+        return self.avatar.url if self.avatar else "{}/user/av_default.png".format(settings.STATIC_URL)

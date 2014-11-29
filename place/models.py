65fbce9ad781b90561c4f985b69a5a1e6702de2d
Ilya Korovin <ilya.krvn@gmail.com>
2014-11-29 20:51:10 +0400

    + flags everywhere

--- place/models.py
+++ place/models.py
@@ -1,10 +1,15 @@
 from django.db import models
 from django.contrib import admin
+import tripnsale.settings as settings
+
 
 class Country(models.Model):
     title = models.TextField(default="")
     ititle = models.TextField(default="")
     name = models.TextField(default="")
+
+    def img(self):
+        return "{}/countries/{}_preview.gif".format(settings.STATIC_URL, self.name)
 
     def __str__(self):
         return self.title

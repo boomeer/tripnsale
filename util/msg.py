bffaf02738ef4ff50dc561254dfa8085cf63d957
Ilya Uvarenkov <trukanduk@gmail.com>
2014-12-20 05:42:40 +0300

    auth fixes

--- util/msg.py
+++ util/msg.py
@@ -13,6 +13,7 @@ def GetRegMsg(name):
         "password_is_invalid": "Неверный символ в пароле О_О. Как Вы этого добились!?",
         "bad_password_len": "Пароль должен иметь от 3 до 30 символов",
         "passwords_are_not_equal": "Пароли не совпадают",
+        "offer_wasnt_be_agreed": "Вы должны согласиться с положением оферты",
     }.get(name, "")
 
 def GetBuyEditMsg(name):

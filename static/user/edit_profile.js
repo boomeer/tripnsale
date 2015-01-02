a2bcd0354730f2e53fbc92bac98d47786d82fe9b
Ilya Uvarenkov <trukanduk@gmail.com>
2015-01-02 15:45:25 +0300

    profile_edit datepickers improved + style of read-only DP moved to base.less

--- static/user/edit_profile.js
+++ static/user/edit_profile.js
@@ -1,7 +1,11 @@
 $(function() {
-    $("#inputBday").datepicker({
-        dateFormat: "dd.mm.yy"
-    });
-
+    $.datepicker.setDefaults($.datepicker.regional['ru']);
+    $("#inputBday").datepicker(
+        {
+            "dateFormat": "dd.mm.yy",
+            "maxDate": "-16y",
+            "changeYear": true,
+            "changeMonth": true
+        });
     CountriesAC("#inputCountry");
 });

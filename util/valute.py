4cf5cdb118669c493947c8ccd4d2068f9cce6ec1
Ilya Uvarenkov <trukanduk@gmail.com>
2015-01-03 19:16:00 +0300

    improvements for python 2/3

--- util/valute.py
+++ util/valute.py
@@ -1,18 +1,26 @@
 #!/usr/bin/env python
 
 import xml.etree.ElementTree as xmltree
-import urllib.request
+try:
+    from urllib.request import urlopen
+except ImportError:
+    from urllib2 import urlopen
 from valute.models import Valute
 from datetime import datetime
 
 def UpdateRates(url="http://www.cbr.ru/scripts/XML_daily.asp"):
-    response = urllib.request.urlopen(url)
+    response = urlopen(url)
 
     text = response.read().decode('cp1251')
     curTime = datetime.now()
     cbr = xmltree.fromstring(text)
     updDate = datetime.strptime(cbr.get("Date"), "%d.%m.%Y")
+    print ("now is {}, xml updated at {}.".format(curTime, updDate))
     for valute in cbr.findall('Valute'):
+        print ("{name}->RUB: {nominal}->{value}".format(
+                name=valute.find("CharCode").text,
+                nominal=float(valute.find("Nominal").text.replace(',', '.')),
+                value=float(valute.find("Value").text.replace(',', '.'))))
         v = Valute(
             date=updDate,
             updTime=curTime,
@@ -21,6 +29,8 @@ def UpdateRates(url="http://www.cbr.ru/scripts/XML_daily.asp"):
             fromId=valute.find("CharCode").text,
             toVal=float(valute.find("Value").text.replace(',', '.')))
         v.save()
+
+    print ("")
 
 if __name__ == "__main__":
     UpdateRates()

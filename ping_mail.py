0551fc852bc12a505f44178cacb31235dd441661
Ilya Uvarenkov <trukanduk@gmail.com>
2015-01-24 22:39:56 +0300

    improved ping_mail.py util

--- ping_mail.py
+++ ping_mail.py
@@ -4,9 +4,8 @@ os.environ.setdefault("DJANGO_SETTINGS_MODULE", "tripnsale.settings")
 from mail.utils import SendMail
 import django
 import sys
-
+django.setup()
 
 if __name__ == "__main__":
-    django.setup()
     print("sending...")
-    SendMail("info@tripnsale.com", sys.argv[1], "it's dummy")
+    SendMail(sys.argv[1], "mail/test.html")
